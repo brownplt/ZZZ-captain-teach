88200665e60f19f77a11d5770999703280732f14
Joe Gibbs Politz <joe@cs.brown.edu>
2013-06-06 15:19:03 -0400

    design recipe sketch

--- src/static/models/encounter.js
+++ src/static/models/encounter.js
@@ -1,51 +1,105 @@
 "use strict";
 
 var Encounter = Backbone.Model.extend({
-    render: function (container) {
-        throw "trying to render encounter without render function";
-    },
+  render: function (container) {
+    throw "trying to render encounter without render function";
+  },
 
-    save: function () {
-        throw "trying to save encounter without save function";
-    },
+  save: function () {
+    throw "trying to save encounter without save function";
+  },
 
-    get_results: function () {
-        return {};
-    },
+  getResults: function () {
+    return {};
+  },
 
-    is_complete: function () {
-        throw "trying to call is_complete encounter without is_complete function";
-    },
+  isComplete: function () {
+    throw "trying to call is_complete encounter without is_complete function";
+  },
 
-    get_thumbnail: function () {
-        throw "trying to call get_thumbnail encounter without that function";
-    }
+  getThumbnail: function () {
+    throw "trying to call get_thumbnail encounter without that function";
+  }
 });
 
 // code, instructions
 var ReplEncounter = Encounter.extend({
-    render: function (container) {
-        var instructions = jQuery("<div>");
-        instructions.html(this.instructions);
-        
-        var replContainer = jQuery("<div>");
-        var codeContainer = jQuery("<div>");
-        container.append(codeContainer);
-        container.append(replContainer);
+  render: function (container) {
+    var instructions = jQuery("<div>");
+    instructions.html(this.instructions);
+    
+    var replContainer = jQuery("<div>");
+    var codeContainer = jQuery("<div>");
+    container.append(codeContainer);
+    container.append(replContainer);
 
-        var runFun = makeRepl(replContainer);
-        this.editor = makeEditor(codeContainer,
-                                 { initial: this.code,
-                                   run: runFun });
-    },
+    var runFun = makeRepl(replContainer);
+    this.editor = makeEditor(codeContainer,
+                             { initial: this.code,
+                               run: runFun });
+  },
 
-    save: function () {
-        this.code = this.editor.getValue();
-        // NOTE(dbp): we need to rig up persistence for this to work.
-        // Backbone.Model.prototype.save.apply(this);
-    },
+  save: function () {
+    this.code = this.editor.getValue();
+    // NOTE(dbp): we need to rig up persistence for this to work.
+    // Backbone.Model.prototype.save.apply(this);
+  },
 
-    get_results: function () {
+  getResults: function () {
 
-    }
+  }
 });
+
+
+var StepSpec = Backbone.Model.extend({ });
+/*
+  constructed with:
+  {
+    instructions: DOM,
+    initialCode: PyretString,
+    reviewed: Bool,
+  }
+*/
+
+var StepData = Backbone.Model.extend({ });
+/*
+  constructed with:
+  {
+    encounter: Encounter U undefined,
+    reviews: 
+  }
+*/
+
+
+var DATA = 'data', EXAMPLES = 'examples', TEMPLATE = 'template',
+    HEADER = 'header', CHECK = 'check', FUNCTION = 'function';
+var STAGES = [DATA, EXAMPLES, TEMPLATE, HEADER, CHECK, FUNCTION];
+var DesignRecipeEncounter = Encounter.extend({
+/*
+  constructed with:
+  {
+    name: String,
+    instructions: DOM,
+    specStages: { [data, examples, template, header, check, function] : StepSpec },
+    userStages: { [data, examples, template, header, check, function] : StepData }
+  }
+  also has:
+  {
+    state: STAGES
+  }
+*/
+  initialize: function() {
+    this.validate();
+    this.state = DATA;
+  },
+
+  validate: function () {
+    _.keys(this.encounters).intersection(STAGES).length === STAGES.length;
+  },
+
+  render: function (container) {
+    
+  }
+  
+});
+
--- src/tests/spec/models/encounter.js
+++ src/tests/spec/models/encounter.js
@@ -1,24 +1,32 @@
 describe("Encounter", function() {
-  var encounter;
 
-  beforeEach(function() {
-    encounter = new ReplEncounter({code: "fun foo(): 'bar' end",
-                              instructions: "These are the instructions"});
+  describe("ReplEncounter", function() {
+    var encounter;
+
+    beforeEach(function() {
+      encounter = new ReplEncounter({code: "fun foo(): 'bar' end",
+                                instructions: "These are the instructions"});
+    });
+
+    it("should be able to render itself", function() {
+      var div = jQuery("<div>");
+      encounter.render(div);
+      expect(div[0].childNodes.length).not.toEqual(0);
+    });
+
+    it("should update it's code attribute on save", function() {
+      var msg = "ARRRRRRRRRRRRRRRR!!!!";
+      encounter.render(jQuery("<div>"));
+      encounter.editor.setValue(msg);
+      encounter.save();
+      expect(encounter.code).toEqual(msg);
+    });
   });
 
-  it("should be able to render itself", function() {
-    var div = jQuery("<div>");
-    encounter.render(div);
-    expect(div[0].childNodes.length).not.toEqual(0);
-  });
-
-  it("should update it's code attribute on save", function() {
-    var msg = "ARRRRRRRRRRRRRRRR!!!!";
-    encounter.render(jQuery("<div>"));
-    encounter.editor.setValue(msg);
-    encounter.save();
-    expect(encounter.code).toEqual(msg);
-  });
-
+  describe("DesignRecipeEncounter", function() {
+    var encounter;
     
+
+  });
+
 });
