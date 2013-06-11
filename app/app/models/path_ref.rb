4d85d99451586935e810abe0b416e5ecc354b92e
Joe Gibbs Politz <joe@cs.brown.edu>
2013-06-11 15:34:11 -0400

    initial methods for path_ref

--- app/app/models/path_ref.rb
+++ app/app/models/path_ref.rb
@@ -1,5 +1,29 @@
 class PathRef < ActiveRecord::Base
 
-  belongs_to :user_repo
+  class FileExists < Exception
+    attr :path
+    def initialize(path)
+      super
+      @path = path
+    end
+  end
+
+  belongs_to :user_repo, :foreign_key => :repo_id
+
+  attr_accessor :path, :user_repo
+
+  def file_exists?
+    user_repo.has_file_head?(@path)
+  end
+
+  def create_file(contents, user)
+    raise FileExists.new(@path) if file_exists?
+    user_repo.create_file(@path, contents, "File created", user)
+  end
+
+  def contents
+    user_repo.lookup_file_head(@path)
+  end
 
 end
+
--- app/spec/models/path_ref_spec.rb
+++ app/spec/models/path_ref_spec.rb
@@ -6,13 +6,61 @@ describe PathRef do
     @TMPDIR = "/tmp/path_ref_tests/"
     @test_dir = @TMPDIR + "test/"
 
+    Dir.mkdir(@TMPDIR)
+
     @test_repo = UserRepo.init_repo(@test_dir)
+
+    @test_file1 = "assignment1.arr"
+    @test_file2 = "assignment2.arr"
+    @test_file_exists = "assignment3.arr"
+
+    @user = { :name => "Joe", :email => "joe@foobar.com" }
+    @test_repo.create_file(
+      @test_file_exists,
+      "#commentary",
+      "Comment commit",
+      @user)
   end
 
   after(:all) do
     FileUtils.rm_r(Dir.glob(@TMPDIR))
   end
 
+  it "should be constructible even if the path does not exist (yet)" do
+    pr = PathRef.new(:user_repo => @test_repo, :path => @test_file1)
+    pr.path.should(eq(@test_file1))
+    pr.user_repo.should(equal(@test_repo))
+    pr.file_exists?.should(equal(false))
+  end
 
+  it "should be constructible if the path does exist" do
+    pr = PathRef.new(:user_repo => @test_repo, :path => @test_file_exists)
+    pr.path.should(eq(@test_file_exists))
+    pr.user_repo.should(equal(@test_repo))
+    pr.file_exists?.should(equal(true))
+  end
+
+  it "should create the file if it does not exist" do
+    pr = PathRef.new(:user_repo => @test_repo, :path => @test_file1)
+    test_str = "data List: | empty end"
+    commit_before = pr.user_repo.repo.last_commit
+    pr.create_file(test_str, @user)
+    commit_after = pr.user_repo.repo.last_commit
+    pr.contents().should(eq(test_str))
+    commit_before.should_not(eq(commit_after))
+  end
+
+  it "should error on creation if the file already exists" do
+    pr = PathRef.new(:user_repo => @test_repo, :path => @test_file2)
+    test_str = "data List: | empty end"
+    commit_before = pr.user_repo.repo.last_commit
+    pr.create_file(test_str, @user)
+    commit_between = pr.user_repo.repo.last_commit
+
+    expect { pr.create_file(test_str, @user) }.to(raise_error(PathRef::FileExists))
+    commit_final = pr.user_repo.repo.last_commit
+    commit_final.should(eq(commit_between))
+  end
 
 end
+
