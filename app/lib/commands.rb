086992e6f4988e3bf2f2970dc0a42b333648f646
Daniel Patterson <dbp@riseup.net>
2013-06-12 14:06:18 -0400

    start writing actual commands, to start: assignment

--- app/lib/commands.rb
+++ app/lib/commands.rb
@@ -2,20 +2,12 @@ module Commands
 
   class InvalidCommand < Exception
   end
-
-  class Version1
-    def summary (tag)
-      {type: "description", content: tag[:content]}
-    end
+  class InvalidTag < Exception
   end
-
-  # commands is a mapping from [VERSION, "TAGNAME"] to a function
-  # that interprets the content of the specified tag.
-  @versions = {1 => Version1.new()}
 
   def interp_tag(version, name, tag)
     pair = [version,name]
-    if @versions.has_key?(version) and 
+    if @versions.has_key?(version) and
        @versions[version].respond_to?(name)
       @versions[version].send(name, tag)
     else
@@ -23,4 +15,38 @@ module Commands
     end
   end
   module_function :interp_tag
+
+  class Version1
+    def assignment(tag)
+      if !tag.has_key?(:tag) or tag[:tag] != "assignment" or
+          !tag.has_key?(:name) or !tag[:name].is_a?(String) or
+          !tag.has_key?(:description) or !tag[:description].is_a?(String) or
+          !tag.has_key?(:instructions) or !tag[:instructions].is_a?(String) or
+          !tag.has_key?(:pieces) or !tag[:pieces].is_a?(Array)
+        raise InvalidTag
+      end
+      pieces = tag[:pieces].each {|t|
+        if !t.is_a?(Hash) or !t.has_key?(:tag)
+          raise InvalidTag
+        end
+        begin
+          Commands::interp_tag(1, t[:tag], t)
+        rescue InvalidCommand => e
+          # an invalid command on a nested tag means this is
+          # an invalid tag
+          raise InvalidTag
+        end
+      }
+      { type: "assignment",
+        name: tag[:name],
+        description: tag[:description],
+        instructions: tag[:instructions],
+        pieces: pieces }
+    end
+  end
+
+  # commands is a mapping from [VERSION, "TAGNAME"] to a function
+  # that interprets the content of the specified tag.
+  @versions = {1 => Version1.new()}
+
 end
--- app/spec/lib/commands_spec.rb
+++ app/spec/lib/commands_spec.rb
@@ -2,21 +2,170 @@ require 'spec_helper'
 
 describe "Commands" do
   it "should explode if an invalid version/name pair is specified" do
-    expect {Commands::interp_tag(-1, "summary", {})}.to(
+    expect {Commands::interp_tag(-1, "assignment", {})}.to(
+     raise_error(Commands::InvalidCommand)
+    )
+    expect {Commands::interp_tag(1, "fooINVALIDTAG", {})}.to(
      raise_error(Commands::InvalidCommand)
     )
   end
   describe "version 1" do
-    describe "summary" do
-      it "should result in an object with type 'description'" do
-        Commands.interp_tag(1,"summary",
-                   {tag: "summary", 
-                    content: "Foo"})[:type].should(eq("description"))
+    VERSION = 1
+    describe "assignment" do
+      it "should error if it is missing tag" do
+        expect {Commands::interp_tag(VERSION, "assignment", {})}.to(
+          raise_error(Commands::InvalidTag)
+        )
       end
-      it "should result in an object with content equal to content" do
-        Commands.interp_tag(1,"summary",
-                   {tag: "summary", 
-                    content: "Foo"})[:content].should(eq("Foo"))
+      it "should error if tag isn't the string 'assignment'" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: ["assignment"]})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "foo"})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if it is missing name" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment"})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if name isn't a string" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: false})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                      name: {}})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if it is missing description" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment"})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if description isn't a string" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: 1})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if description isn't a string" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: [10,10]})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if description isn't a string" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: true})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if it is missing instructions" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment"
+                                     })}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if instructions is not a string" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: 10})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: ["blah"]})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: {string: "foo"}})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if it is missing pieces" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: "Do it all!"})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should error if pieces isn't a list of tags" do
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: "Do it all!",
+                                     pieces: "foo"})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: "Do it all!",
+                                     pieces: ["foo", "bar"]})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: "Do it all!",
+                                     pieces: [{a:"foo"}, {b:"bar"}]})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+        expect {Commands::interp_tag(VERSION, "assignment",
+                                     {tag: "assignment",
+                                     name: "My Assignment",
+                                     description: "The greatest assignment",
+                                     instructions: "Do it all!",
+                                     pieces: [{tag:"fooINVALIDTAG"}]})}.to(
+          raise_error(Commands::InvalidTag)
+        )
+      end
+      it "should produce an object with the same attributes" do
+        name = "My Assignment"
+        description = "The greatest assignment"
+        instructions = "Do it all!"
+        Commands::interp_tag(VERSION, "assignment",
+                             {tag: "assignment",
+                             name: name,
+                             description: description,
+                             instructions: instructions,
+                             pieces: []}).should(
+            eq({type: "assignment",
+               name: name,
+               description: description,
+               instructions: instructions,
+               pieces: []}))
       end
     end
   end
