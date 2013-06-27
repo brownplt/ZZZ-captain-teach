763fa76839b4dc87531d80a1b3e754e38e202ac3
Daniel Patterson <dbp@dbpmail.net>
2013-06-27 01:42:22 -0400

    fix ruby scribble lib so it works all the time (instead of some of the time)

--- web/lib/scribble.rb
+++ web/lib/scribble.rb
@@ -1,18 +1,36 @@
 require 'open3'
 module Scribble
-  def render(path_ref)
-    #scribble_command = "../../../src/scribble/ct-lang-main.rkt"
-    #ct_lang = File.expand_path(scribble_command, __FILE__)
-    source = path_ref.create_temporary
-    dest = Tempfile.new(["scribble", '.html'])
-    path = Pathname.new(dest.path)
-    rv = "NO VALUE"
-    Open3.popen3('scribble', '--dest', path.dirname.to_s, '--dest-name', path.basename.to_s, source.path) do |stdin, stdout, stderr|
-      dest.rewind
-      rv = dest.gets(nil)
-    end
-    rv
+
+  class ScribbleError < Exception
   end
 
+  def render(path_ref)
+    source = path_ref.create_temporary
+    # NOTE(dbp): if we don't end it in .html, scribble adds that
+    # for us.
+    dest = Tempfile.new(["scribble", '.html'])
+    path = Pathname.new(dest.path)
+    # NOTE(dbp): --dest-name is confusing - if you pass it an
+    # absolute path, it essentially basenames it. --dest, on the
+    # other hand, sets the directory. Combining both, we can tell
+    # it where to put the output. NB@joe: trying hard to do your
+    # resolution for the year.
+    Open3.popen3('scribble', '--dest', 
+                 path.dirname.to_s, 
+                 '--dest-name', path.basename.to_s, 
+                 source.path) do |stdin, stdout, stderr, wait|
+      # an ode to "wouldn't it be nice if scribble 
+      # had a stdout option"
+      wait.value # wait for the process to end
+      
+      err = stderr.gets(nil) # pass any errors up
+      if !err.nil?
+        raise ScribbleError,err
+      end
+      dest.rewind # go to the beginning of temp file
+      @rv = dest.gets(nil) # read contents (what scribble wrote)
+    end
+    @rv
+  end
   module_function :render
 end
