ec949ad685026ead771b970d8e6e72469e66f551
Joe Gibbs Politz <joe@cs.brown.edu>
2013-07-03 15:45:21 -0400

    fix some tests to add directories for repos

--- web/blob_spec.rb
+++ web/blob_spec.rb
@@ -2,13 +2,19 @@ require 'spec_helper'
 
 describe FunctionData do
   before(:all) do
-    User.create()
-    @user = User.last()
+    USER_GIT_REPO_PATH = "/tmp/ct-user-repos"
+    Dir.mkdir(USER_GIT_REPO_PATH)
+    @user = User.create!(email: "edward@captainteach.org")
+
     @ref = "foo"
     @fd = FunctionData.new(:user => User.last, :ref => @ref)
     @fd.save()
   end
   
+  after(:all) do
+    FileUtils.rm_rf(USER_GIT_REPO_PATH)
+  end
+
   it "should be able to be looked up by ref and user" do
     FunctionData.find_by(user: @user, ref: @ref).
       should(eq(@fd))
