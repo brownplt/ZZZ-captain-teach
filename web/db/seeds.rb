fabdd87ecd3b89867d1c197680f6a6e5ca103641
Joe Gibbs Politz <joe@cs.brown.edu>
2013-07-19 10:10:36 -0700

    more cleanup on db restart

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -8,28 +8,42 @@
 
 
 if Rails.env.development?
+
+  FileUtils.rm_rf(USER_GIT_REPO_PATH)
+  FileUtils.mkdir(USER_GIT_REPO_PATH)
+  
   captain = User.create!(:email => "edward@captainteach.org")
   captains_log = UserRepo.create!(:path => ASSIGNMENTS_PATH)
 
+  course = Course.create!(:title => "CS001")
+  course.teachers << captain
+  course.save!
+
   path1 = PathRef.create!(:user_repo => captains_log, :path => "multiple-choice.jrny")
   example_assignment1 = Assignment.create!({
-    :path_ref => path1
+    :path_ref => path1,
+    :course => course
   })
 
   path2 = PathRef.create!(:user_repo => captains_log, :path => "function.jrny")
   example_assignment2 = Assignment.create!({
-    :path_ref => path2
+    :path_ref => path2,
+    :course => course
   })
 
   path3 = PathRef.create!(:user_repo => captains_log, :path => "oracle.jrny")
   example_assignment3 = Assignment.create!({
-    :path_ref => path3
+    :path_ref => path3,
+    :course => course
   })
 
   path4 = PathRef.create!(:user_repo => captains_log, :path => "tutorial.jrny")
   example_assignment4 = Assignment.create!({
-    :path_ref => path4
+    :path_ref => path4,
+    :course => course
   })
+
+  print("Visit the demo course at: #{APP_URL}/course/#{course.id}\n")
 
 end
 
