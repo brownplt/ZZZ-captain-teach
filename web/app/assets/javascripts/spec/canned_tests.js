60d9c193e635dc6f1aa646523a986e5d86fd6038
Joe Gibbs Politz <joe@cs.brown.edu>
2013-09-01 19:37:32 -0400

    forgot a test file

---
+++ web/app/assets/javascripts/spec/canned_tests.js
@@ -0,0 +1,14 @@
+
+describe("reviews interactions", function () {
+
+  it("should work", function () {
+    var resources = TESTDATA.user.resources;
+    var args = TESTDATA.user.args;
+    codeAssignment(
+        $("#review-editor1"),
+        TESTDATA.user.resources,
+        TESTDATA.user.args
+    );
+
+  });
+});
