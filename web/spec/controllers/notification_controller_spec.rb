835c47b3d2040d587b6cd9c11bb271839607e9ce
Joe Gibbs Politz <joe@cs.brown.edu>
2013-09-04 17:23:15 -0400

    database updates for dev on postgres

--- web/spec/controllers/notification_controller_spec.rb
+++ web/spec/controllers/notification_controller_spec.rb
@@ -12,22 +12,20 @@ describe NotificationController do
   it "should handle reporting abuse" do
     controller.login_browserid @user.email
 
-    data = {
-        "abuse_data" => {
+    data = JSON.dump({
           "resource" => Resource::mk_resource("b","r","feedback",{},5),
           "type" => "feedback",
           "data" => {
             "comments" => "You suck",
             "helpfullnesss" => -2
           }
-        }
-      }
+        })
 
     post :report_abuse, :abuse_data => data, :format => :json
 
     ar = AbuseRecord.last
     ar.user.should(eq(@user))
-    JSON.parse(ar.abuse_data).should(eq(data))
+    ar.abuse_data.should(eq(data))
 
   end
 
