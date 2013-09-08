328fd9f28490bd645beb76331e16a40deafa9391
Joe Gibbs Politz <joe@cs.brown.edu>
2013-09-08 12:16:18 -0400

    add a flag for global email on/off

--- web/app/mailers/user_mailer.rb
+++ web/app/mailers/user_mailer.rb
@@ -3,13 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def review_email(user, assignment_id, step)
     begin
-      if user.send_email and not (user.send_email.nil?)
+      if SEND_EMAIL and user.send_email and not (user.send_email.nil?)
         @assignment_id = assignment_id
         @step = step
         mail(to: user.email, subject: "You've got a review!").deliver
       end
     rescue => ex
       Airbrake.notify(ex)
+      ActionMailer::Base::NullMail.new
     end
   end
 
